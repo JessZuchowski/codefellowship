26f73c1d4ebf2e4b262102afd00234b8d51b3356
jaezuchowski <jess.zuchowski@gmail.com>
2019-05-07 12:46:45 -0700

    need to work on integration tests and user page

---
+++ src/test/java/com/JessZuchowski/codefellowship/database/ApplicationUserTest.java
@@ -0,0 +1,78 @@
+//package com.JessZuchowski.codefellowship.database;
+//
+//import com.JessZuchowski.codefellowship.security.UserDetailsServiceImpl;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//import static org.junit.Assert.*;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class ApplicationUserTest {
+//
+//    @Autowired
+//    UserDetailsServiceImpl;
+//
+//    @Autowired
+//    UserRepository;
+//
+//
+//    @Test
+//    public void getId() {
+//    }
+//
+//    @Test
+//    public void getAuthorities() {
+//    }
+//
+//    @Test
+//    public void setUsername() {
+//    }
+//
+//    @Test
+//    public void getUsername() {
+//    }
+//
+//    @Test
+//    public void setPassword() {
+//    }
+//
+//    @Test
+//    public void getPassword() {
+//    }
+//
+//    @Test
+//    public void setFirstName() {
+//    }
+//
+//    @Test
+//    public void getFirstName() {
+//    }
+//
+//    @Test
+//    public void setLastName() {
+//    }
+//
+//    @Test
+//    public void getLastName() {
+//    }
+//
+//    @Test
+//    public void setDateOfBirth() {
+//    }
+//
+//    @Test
+//    public void getDateOfBirth() {
+//    }
+//
+//    @Test
+//    public void setBio() {
+//    }
+//
+//    @Test
+//    public void getBio() {
+//    }
+//}
